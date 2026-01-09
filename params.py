f6820496014a2732cba9aeea68962a034a73e9ef
Jessica Sun <jessica.yq.sun@outlook.com>
2026-01-09 13:50:58 +1100

    fix metadata paths in params

--- params.py
+++ params.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 OUTPUT_JSON_FILE_NAME = "output.json"
 OUTPUT_HTML_FILE_NAME = "output.html"
 
@@ -8,6 +9,6 @@ RAW_FILE_TYPES = ['.fastq', '.fasta']
 PROCESSED_FILE_TYPES = ['.cram', '.bam']
 SUMMARISED_FILE_TYPES = ['.vcf', '.maf', '.csv', '.tsv']
 
-METADATA = "sample_metadata\sample_metadata.json"
-SAMPLE_TO_PATIENT = "sample_metadata\sample_to_patient.json"
+METADATA = Path("sample_metadata") / "sample_metadata.json"
+SAMPLE_TO_PATIENT = Path("sample_metadata") / "sample_to_patient.json"
 ORGANIZATION = "National Academy of Taiwan"
